71f9c126304624c24cf8ac144c66e100f5823482
mumohh2 <mumoemmanuel6627@gmail.com>
2023-07-16 12:53:57 -0700

    102-print_comb5

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -11,12 +11,11 @@ int main(void)
 
 	for (p = 0; p <= 98; p++)
 	{
-	
 	for (q = p + 1; q <= 99; q++)
 	{
 	putchar((p / 10) + '0');
 	putchar((p % 10) + '0');
-	putchar(',');
+	putchar(' ');
 	putchar((q / 10) + '0');
 	putchar((q % 10) + '0');
 	if (p == 98 && q == 99)
