4edd882b3335e168d62b67ba5ef194e0c0028940
mumohh2 <mumoemmanuel6627@gmail.com>
2023-07-24 12:07:22 -0700

    4-print_rev.c

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * print_rev - imprime en reversa
+ * @s: string
+ * Return: 0
+ */
+void print_rev(char *s)
+{
+	int longi = 0;
+	int 0;
+
+	while (*s != '\0')
+	{
+		longi++;
+		s++;
+	}
+	s--;
+	for (0 = longi; 0 > 0; 0--)
+	{
+		_putchar(*s);
+		s--;
+	}
+	_putchar('\n');
+}
