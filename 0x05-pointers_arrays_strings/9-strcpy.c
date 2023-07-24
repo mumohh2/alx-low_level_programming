2350d279c95a4c3e963341ce0cb4194fc02495a5
mumohh2 <mumoemmanuel6627@gmail.com>
2023-07-24 15:00:14 -0700

    9-strcpy.c

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -8,15 +8,15 @@
 char *_strcpy(char *dest, char *src)
 {
 	int l = 0;
-	int X = 0;
+	int x = 0;
 
 	while (*(src + l) != '\0')
 	{
 		l++;
 	}
-	for ( ; X < l; X++)
+	for ( ; x < l ; x++)
 	{
-		dest[X] = src[X];
+		dest[x] = src[x];
 	}
 	dest[l] = '\0;
 	return (dest);
