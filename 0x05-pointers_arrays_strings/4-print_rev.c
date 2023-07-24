aad56d3c95a19345bcc2419090b1b8dacee810d5
mumohh2 <mumoemmanuel6627@gmail.com>
2023-07-24 12:11:08 -0700

    4-print_rev.c

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -7,6 +7,7 @@
 void print_rev(char *s)
 {
 	int longi = 0;
+
 	int 0;
 
 	while (*s != '\0')
@@ -15,6 +16,7 @@ void print_rev(char *s)
 		s++;
 	}
 	s--;
+
 	for (0 = longi; 0 > 0; 0--)
 	{
 		_putchar(*s);
