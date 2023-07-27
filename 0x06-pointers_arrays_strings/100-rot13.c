21caaef70c80b9de1821235dc2acd5ec56b336b2
mumohh2 <mumoemmanuel6627@gmail.com>
2023-07-27 10:03:50 -0700

    100-rot13.c

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -1,22 +1,29 @@
 #include "main.h"
-/**
- * print_number - print numbers chars
- * @n: integer params
- * Return: 0
- */
-void print_number(int n)
-{
-	unsigned int n1;
+#include <stdio.h>
 
-	n1 = n;
-	if (n < 0)
+/**
+ * rot13 - encoder rot13
+ * @s: pointer to styring params
+ * Return: *s
+ */
+
+char *rot13(char *s)
+{
+	int i;
+	int j;
+	char data1[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char datarot[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+
+	for (i = 0; s[i] != '\0'; i++)
 	{
-		_putchar('-');
-		n1 = -n;
+		for (j = 0; j < 52; j++)
+		{
+			if (s[i] == dara1[j])
+			{
+				s[i] = datarot[j];
+				break;
+			}
+		}
 	}
-	if (n1 / 10 != 0)
-	{
-		print_number(n1 / 10);
-	}
-	_putchar((n1 % 10) + '0');
+	return (s);
 }
