f8ba33f9a3f544fa958e454d9e64bbf584b8bb70
mumohh2 <mumoemmanuel6627@gmail.com>
2023-08-08 14:32:55 -0700

    1-strdup.c

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -13,7 +13,7 @@ char *_strdup(char *str)
 
 	if (str == NULL)
 		return (NULL);
-	i == 0;
+	i = 0;
 	while (str[i] != '\0')
 		i++;
 	aaa = malloc(sizeof(char) * (1 + 1));
@@ -23,4 +23,6 @@ char *_strdup(char *str)
 
 	for (r = 0; str[r]; r++)
 		aaa[r] = str[r];
+
+	return (aaa);
 }
