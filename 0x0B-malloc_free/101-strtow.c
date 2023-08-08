e5eac19a16510cb046b910d25a9f828ec371dd4d
mumohh2 <mumoemmanuel6627@gmail.com>
2023-08-08 16:59:22 -0700

    101-strtow.c

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -37,9 +37,9 @@ char **strtow(char *str)
 
 	while (*(str + len))
 		len++;
-	words = count_word(str)
-		if (words == 0)
-			return (NULL);
+	words = count_word(str);
+	if (words == 0)
+		return (NULL);
 
 	matrix = (char **) malloc(sizeof(char *) * (words + 1));
 	if (matrix == NULL)
