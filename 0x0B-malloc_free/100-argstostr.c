f523754c793fac9b0651a09812e0fd58a97f77a7
mumohh2 <mumoemmanuel6627@gmail.com>
2023-08-08 16:13:07 -0700

    100-argstostr.c

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -8,7 +8,7 @@
  */
 char *argstostr(int ac, char **av)
 {
-	int i, n, r = 0, 1 = 0;
+	int i, n, r = 0, l = 0;
 	char *str;
 
 	if (ac == 0 || av == NULL)
@@ -17,11 +17,11 @@ char *argstostr(int ac, char **av)
 	for (i = 0; i < ac; i++)
 	{
 		for (n = 0; av[i][n]; n++)
-			1++;
+			l++;
 	}
-	1 += ac;
+	l += ac;
 
-	str = malloc(sizeof(char) * 1 + 1);
+	str = malloc(sizeof(char) * l + 1);
 	if (str == NULL)
 		return (NULL);
 	for (i = 0; i < ac; i++)
