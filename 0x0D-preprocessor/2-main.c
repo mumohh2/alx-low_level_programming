9ef6dee0a7a77517ce1ee787a78159497c491600
mumohh2 <mumoemmanuel6627@gmail.com>
2023-08-14 04:00:30 -0700

    2-main.c

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * main - prints the name of the file of the program was compiled from
+ * main - prints the name of the file program was compiled from
  * Return: Always 0
  */
 int main(void)
