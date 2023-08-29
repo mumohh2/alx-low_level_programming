c013a777dc5eba1159d10a5c8c3391aa05eb9272
mumohh2 <mumoemmanuel6627@gmail.com>
2023-08-29 03:45:41 -0700

    lists.h

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,4 +1,5 @@
 #include "lists.h"
+#include <stdlib.h>
 /**
  * print_listint - print all the elements of a linked list
  * @h: linked list of type listint_t to print
--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -2,7 +2,7 @@
 #define LIST_H
 
 #include <stdio.h>
-#include <stdlib>
+#include <stdlib.h>
 
 /**
  * struct listint_s - singly linked list
