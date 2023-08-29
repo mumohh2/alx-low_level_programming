e647c654ad47a28dd56467e6c5f56adb7e9ae2d8
mumohh2 <mumoemmanuel6627@gmail.com>
2023-08-29 03:27:15 -0700

    0-print_listint.c

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+/**
+ * print_listint - print all the elements of a connected list
+ * @h: connected list of type listint_t to print
+ * Return: number of nodes
+ */
+size_t print_listint(const listint_t *h)
+{
+	size_t num = 0;
+
+	while (h)
+	{
+		printf("%d\n", h->n);
+		num++;
+		h = h->next;
+	}
+	return (num);
+}
